661b287748316b3c730f51e26d8783c3dc4aeeab
Sargun Vohra <sargunv@users.noreply.github.com>
2026-01-24 02:52:34 -0800

    cli: update identify help text with complete format list

--- internal/cli/identify/root.go
+++ internal/cli/identify/root.go
@@ -25,25 +25,28 @@ var Cmd = &cobra.Command{
 
 Supports:
 - Platform specific ROMs: identifies game information from the ROM header. Supported formats:
-  - Famicom: .nes
-  - Super Famicom: .sfc, .smc
+  - Famicom (NES): .nes
+  - Super Famicom (SNES): .sfc, .smc
   - Nintendo 64: .z64, .v64, .n64
   - Nintendo GameCube / Wii: .gcm, .iso, .rvz, .wia
   - Nintendo Game Boy / Color: .gb, .gbc
   - Nintendo Game Boy Advance: .gba
   - Nintendo DS: .nds, .dsi, .ids
-  - Sega Master System: .sms
-  - Sega Mega Drive / Genesis: .md, .gen, .smd
-  - Sega Saturn: .bin
-  - Sega Dreamcast: .bin
-  - Sega Game Gear: .gg
-  - Sony PlayStation 1: .bin
-  - Sony PlayStation 2: .iso, .bin
-  - Sony PlayStation Portable: .iso
-  - Microsoft Xbox: .iso, .xbe
+  - Nintendo 3DS: .3ds, .cci
+  - Sega Master System / Game Gear: .sms, .gg
+  - Sega Mega Drive (Genesis): .md, .gen, .smd, .32x
+  - Sega CD: .bin, .chd
+  - Sega Saturn: .bin, .chd
+  - Sega Dreamcast: .bin, .chd
+  - Sony PlayStation 1: .bin, .chd
+  - Sony PlayStation 2: .iso, .bin, .chd
+  - Sony PlayStation 3: .pkg
+  - Sony PlayStation Portable: .iso, .chd
+  - Sony PlayStation Vita: .pkg
+  - Microsoft Xbox: .iso, .chd, .xbe
 - .chd discs: extracts SHA1 hashes from header (no decompression needed)
-- .zip archives: identifies contents using ZIP metadata for hashes (CRC32)
-- All files: calculates SHA1, MD5, CRC32 for files under --max-hash-size
+- .zip archives: extracts CRC32 hashes from metadata (no decompression needed)
+- All files: calculates SHA1, MD5, CRC32 for uncompressed files under --max-hash-size
 - All folders: identifies files within`,
 	Args: cobra.MinimumNArgs(1),
 	RunE: runIdentify,
